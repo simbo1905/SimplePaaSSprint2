df0cd592fd99b59a9574ef07fec3425c00bdc360
simbo1905 <no.one@no.where.io>
2017-10-28 23:33:02 +0100

    works

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 REPO=$1
 FILE=$2
+echo curl -k -v -u admin:admin123 --upload-file $FILE  https://nexus.demo.dfe.secnix.co.uk/repository/$REPO/
 curl -k -v -u admin:admin123 --upload-file $FILE  https://nexus.demo.dfe.secnix.co.uk/repository/$REPO/
